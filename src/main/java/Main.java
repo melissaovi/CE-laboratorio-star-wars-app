beaed5d9e865178ff3afbb279420f0452f9563e5
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2021-03-12 19:16:29 -0600

    Added parsing logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,39 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException, IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try {
+            HttpGet request = new HttpGet("https://swapi.dev/api/people");
+            CloseableHttpResponse response = httpClient.execute(request);
+            try {
+                System.out.println(response.getProtocolVersion());              // HTTP/1.1
+                System.out.println(response.getStatusLine().getStatusCode());   // 200
+                System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+                System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    var result = EntityUtils.toString(entity);
+                    ApiResponse<Character> parsedResponse = mapper.readValue(
+                            result, mapper.getTypeFactory().constructParametricType(ApiResponse.class, Character.class));
+                    System.out.println(result);
+                }
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpClient.close();
+        }
     }
 }
